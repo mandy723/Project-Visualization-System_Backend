7215003f182358db94ab04723288feb41f3f3a31
rvs-team <t109598097@ntut.org.tw>
2020-10-24 17:29:51 +0800

    🚧 JSON parsing with GraphQL

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,58 +1,78 @@
 package controller;
 
-import mockData.Product;
-import org.eclipse.egit.github.core.IRepositoryIdProvider;
-import org.eclipse.egit.github.core.Repository;
-import org.eclipse.egit.github.core.RepositoryCommit;
-import org.eclipse.egit.github.core.RepositoryId;
-import org.eclipse.egit.github.core.client.GitHubClient;
-import org.eclipse.egit.github.core.service.CommitService;
-import org.eclipse.egit.github.core.service.RepositoryService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
+import javax.xml.ws.Response;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductController{
 
     @GetMapping("/products/{id}")
-    public Product getProduct(@PathVariable("id") String id) throws IOException {
-        //Basic authentication
-//        GitHubClient client = new GitHubClient();
-//        client.setCredentials("user", "passw0rd");
+    public String getProduct(@PathVariable("id") String id) throws IOException {
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.setErrorHandler(new CustomErrorHandler());
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer 32290bf842eef38efd61707705f5b01aa2f6e8bb");
+            headers.add("content-type", "application/json"); // just modified graphql into json
+
+        String query = "{query: {"+
+            "repository(owner: \"facebook\", name:\"react\") {"+
+                "description"+
+            "}"+
+        "}}";
+
+//            String query = "query: query{" +
+//                                "repository(owner: \"facebook\", name:\"react\") {" +
+//                                    "defaultBranchRef {" +
+//                                        "target {" +
+//                                            "... on Commit {" +
+//                                                "history (first:100) {" +
+//                                                    "nodes {" +
+//                                                        "committedDate" +
+//                                                    "}" +
+//                                                "}" +
+//                                            "}" +
+//                                        "}" +
+//                                    "}" +
+//                                "}" +
+//                            "}";
 
 
-        RepositoryService repositoryService = new RepositoryService();
-
-        IRepositoryIdProvider repoId = new RepositoryId("imper0502","RVS-springboot");
-        Repository repo = repositoryService.getRepository(repoId);
-        System.out.println(" URL: " + repo.getGitUrl());
+            String URL = "https://api.github.com/graphql";
 
 
-        CommitService commitService = new CommitService();
-        List<RepositoryCommit> commits = commitService.getCommits(repoId);
-        System.out.println(" Commits Author: " + commits.get(0).getCommit().getMessage());
-//        for (Repository repo : repositoryService.getRepositories("defunkt")){
-//            commitService( repo.getId(), "master");
-//            System.out.println(" URL: " + repo.getGitUrl());
-//            System.out.println(repo.getName() + " Watchers: " + repo.getWatchers());
-//        }
+            ResponseEntity<String> response = restTemplate.postForEntity(URL, new HttpEntity<>(query, headers), String.class);
+            System.out.println("The response=================" + response);
 
-
-
-
-        Product product = new Product();
-        product.setId(id);
-        product.setName("Romantic Story");
-        product.setPrice(200);
-
-        return product;
+        System.out.println(response.getBody());
+        return response.toString();
     }
 
+}
+
+class CustomErrorHandler implements ResponseErrorHandler {
+
+    @Override
+    public boolean hasError(ClientHttpResponse response) throws IOException {
+        return true;
+    }
+
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+
+    }
 }
