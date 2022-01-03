a3600e077b8595aa33c51fe253cea190988b16c1
Zachary <tsai880806@gmail.com>
2022-01-03 21:39:27 +0800

    Added deleteProject test case in ProjectServiceTest

--- src/test/java/pvs/app/service/ProjectServiceTest.java
+++ src/test/java/pvs/app/service/ProjectServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.parameters.P;
 import org.springframework.test.context.junit4.SpringRunner;
 import pvs.app.Application;
 import pvs.app.dao.ProjectDAO;
+import pvs.app.dao.RepositoryDAO;
 import pvs.app.dto.CreateProjectDTO;
 import pvs.app.dto.ResponseProjectDTO;
 import pvs.app.entity.Project;
@@ -21,7 +22,6 @@ import java.io.IOException;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
@@ -36,11 +36,22 @@ public class ProjectServiceTest {
     @MockBean
     private ProjectDAO projectDAO;
 
+    @MockBean
+    private RepositoryDAO repositoryDAO;
+
     CreateProjectDTO projectDTO;
 
     Project project;
-    Repository githubRepository;
+    Project project2;
+
     Set<Repository> repositorySet;
+    Set<Repository> repositorySet2;
+
+    Repository githubRepository;
+    Set<Project> projectSet;
+
+    Repository githubRepositoryWithMultipleProjects;
+    Set<Project> projectSetWithMultipleProjects;
 
     final String responseJson = "{\"avatarUrl\":\"https://avatars3.githubusercontent.com/u/17744001?u=038d9e068c4205d94c670d7d89fb921ec5b29782&v=4\"}";
     Optional<JsonNode> mockAvatar;
@@ -57,14 +68,39 @@ public class ProjectServiceTest {
         project.setMemberId(1L);
         project.setName(projectDTO.getProjectName());
 
+        project2 = new Project();
+        project2.setProjectId(1L);
+        project2.setMemberId(1L);
+        project2.setName(projectDTO.getProjectName());
+
+        repositorySet2 = new HashSet<>();
+        repositorySet2.add(githubRepositoryWithMultipleProjects);
+        project2.setRepositorySet(repositorySet2);
+
         githubRepository = new Repository();
         githubRepository.setType("github");
         githubRepository.setUrl("https://github.com/facebook/react");
         githubRepository.setRepositoryId(1L);
 
+        githubRepositoryWithMultipleProjects = new Repository();
+        githubRepositoryWithMultipleProjects.setType("github");
+        githubRepositoryWithMultipleProjects.setUrl("https://github.com/SEWinWinWin/PVS_backend");
+        githubRepositoryWithMultipleProjects.setRepositoryId(1L);
+
+        projectSetWithMultipleProjects = new HashSet<>();
+        projectSetWithMultipleProjects.add(project);
+        projectSetWithMultipleProjects.add(project2);
+        githubRepositoryWithMultipleProjects.setProjectSet(projectSetWithMultipleProjects);
+
         repositorySet = new HashSet<>();
         repositorySet.add(githubRepository);
+        repositorySet.add(githubRepositoryWithMultipleProjects);
         project.setRepositorySet(repositorySet);
+
+        projectSet = new HashSet<>();
+        projectSet.add(project);
+        githubRepository.setProjectSet(projectSet);
+
 
         ObjectMapper mapper = new ObjectMapper();
         mockAvatar = Optional.ofNullable(mapper.readTree(responseJson));
@@ -73,8 +109,8 @@ public class ProjectServiceTest {
     @Test
     public void create() throws IOException {
         //context
-//        when(githubApiService.getAvatarURL("facebook"))
-//                .thenReturn(mockAvatar.orElse(null));
+        when(githubApiService.getAvatarURL("facebook"))
+                .thenReturn(mockAvatar.orElse(null));
 
         when(projectDAO.save(any(Project.class)))
                 .thenReturn(project);
@@ -92,7 +128,7 @@ public class ProjectServiceTest {
     public void getMemberProjects() {
         //given
         project.setAvatarURL("https://avatars3.githubusercontent.com/u/17744001?u=038d9e068c4205d94c670d7d89fb921ec5b29782&v=4");
-        
+
         List<ResponseProjectDTO> projectDTOList = new ArrayList<>();
         ResponseProjectDTO projectDTO = new ResponseProjectDTO();
         projectDTO.setProjectId(project.getProjectId());
@@ -107,5 +143,23 @@ public class ProjectServiceTest {
         //then
         assertEquals(1, projectService.getMemberProjects(1L).size());
 //        assertTrue(projectDTOList.equals(projectService.getMemberProjects(1L)));
+    }
+
+    @Test
+    public void deleteProject() {
+        when(projectDAO.findById(1L))
+                .thenReturn(Optional.of(project));
+        doNothing().when(repositoryDAO).deleteById(1L);
+        when(repositoryDAO.save(githubRepositoryWithMultipleProjects)).thenReturn(githubRepositoryWithMultipleProjects);
+        doNothing().when(projectDAO).deleteById(1L);
+
+        try {
+            projectService.deleteProject(1L);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        verify(repositoryDAO, times(1)).deleteById(1L);
+        verify(repositoryDAO, times(1)).save(githubRepositoryWithMultipleProjects);
+        verify(projectDAO, times(1)).deleteById(1L);
     }
 }
