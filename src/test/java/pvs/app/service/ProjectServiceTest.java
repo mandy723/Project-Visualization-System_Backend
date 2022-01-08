143e84a1d398539b7a9a84894715947465fa72c4
Zachary <tsai880806@gmail.com>
2022-01-09 02:50:16 +0800

    Change DB_CONNECT_IP in application.yml

--- src/test/java/pvs/app/service/ProjectServiceTest.java
+++ src/test/java/pvs/app/service/ProjectServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.parameters.P;
 import org.springframework.test.context.junit4.SpringRunner;
 import pvs.app.Application;
 import pvs.app.dao.ProjectDAO;
